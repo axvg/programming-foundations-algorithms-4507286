2c097fe39c51556f74a2334227169de96653c7c9
av <91773368+axvg@users.noreply.github.com>
2024-03-13 21:32:38 +0000

    Dictionary

--- Start/DataStructures/Dictionary/dictionary_start.py
+++ Start/DataStructures/Dictionary/dictionary_start.py
@@ -3,15 +3,21 @@
 
 
 # TODO: create a dictionary all at once
-
+items1 = dict({"k1": 1, "k2": 2, "k3": 3})
+print(items1)
 
 # TODO: create a dictionary progressively
-
+items2 = {}
+items2["k1"] = 1
+items2["k2"] = 2
+print(items2)
 
 # TODO: replace an item
-
+items2["k2"] = "two"
 
 # TODO: try to access a nonexistent key
-
+# items2["k3"]
 
 # TODO: iterate the keys and values in the dictionary
+for k, v in items2.items():
+  print(f"key: {k}, value: {v}")
